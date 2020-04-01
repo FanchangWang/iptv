291a38659e820a7fa842a565ddf29f0bb13d6f9d
guyuexuan <2651919937@qq.com>
2020-04-01 13:06:14 +0800

    fix cctv-4 vid change vid 02 to 03

--- iptv_ysp.php
+++ iptv_ysp.php
@@ -1,7 +1,7 @@
 <?php
 $tvg_list = [
     [
-        "vid" => 2000210102,
+        "vid" => 2000210103,
         "tvg-id" => "1",
         "tvg-name" => "CCTV1",
         "group-title" => "央视",
@@ -9,7 +9,7 @@ $tvg_list = [
         "name" => "CCTV-1综合"
     ],
     [
-        "vid" => 2000203602,
+        "vid" => 2000203603,
         "tvg-id" => "2",
         "tvg-name" => "CCTV2",
         "group-title" => "央视",
@@ -17,7 +17,7 @@ $tvg_list = [
         "name" => "CCTV-2财经"
     ],
     [
-        "vid" => 2000203802,
+        "vid" => 2000203803,
         "tvg-id" => "3",
         "tvg-name" => "CCTV3",
         "group-title" => "央视",
@@ -25,7 +25,7 @@ $tvg_list = [
         "name" => "CCTV-3综艺"
     ],
     [
-        "vid" => 2000266302,
+        "vid" => 2000204803,
         "tvg-id" => "4",
         "tvg-name" => "CCTV4",
         "group-title" => "央视",
@@ -33,7 +33,7 @@ $tvg_list = [
         "name" => "CCTV-4中文国际"
     ],
     [
-        "vid" => 2000205102,
+        "vid" => 2000205103,
         "tvg-id" => "5",
         "tvg-name" => "CCTV5",
         "group-title" => "央视",
@@ -41,7 +41,7 @@ $tvg_list = [
         "name" => "CCTV-5体育"
     ],
     [
-        "vid" => 2000204502,
+        "vid" => 2000204503,
         "tvg-id" => "6",
         "tvg-name" => "CCTV5+",
         "group-title" => "央视",
@@ -49,7 +49,7 @@ $tvg_list = [
         "name" => "CCTV-5+体育赛事"
     ],
     [
-        "vid" => 2000203302,
+        "vid" => 2000203303,
         "tvg-id" => "7",
         "tvg-name" => "CCTV6",
         "group-title" => "央视",
@@ -57,7 +57,7 @@ $tvg_list = [
         "name" => "CCTV-6电影"
     ],
     [
-        "vid" => 2000510002,
+        "vid" => 2000510003,
         "tvg-id" => "8",
         "tvg-name" => "CCTV7",
         "group-title" => "央视",
@@ -65,7 +65,7 @@ $tvg_list = [
         "name" => "CCTV-7国防军事"
     ],
     [
-        "vid" => 2000203902,
+        "vid" => 2000203903,
         "tvg-id" => "9",
         "tvg-name" => "CCTV8",
         "group-title" => "央视",
@@ -73,7 +73,7 @@ $tvg_list = [
         "name" => "CCTV-8电视剧"
     ],
     [
-        "vid" => 2000499402,
+        "vid" => 2000499403,
         "tvg-id" => "10",
         "tvg-name" => "CCTV9",
         "group-title" => "央视",
@@ -81,7 +81,7 @@ $tvg_list = [
         "name" => "CCTV-9记录"
     ],
     [
-        "vid" => 2000203502,
+        "vid" => 2000203503,
         "tvg-id" => "11",
         "tvg-name" => "CCTV10",
         "group-title" => "央视",
@@ -89,7 +89,7 @@ $tvg_list = [
         "name" => "CCTV-10科教"
     ],
     [
-        "vid" => 2000204102,
+        "vid" => 2000204103,
         "tvg-id" => "12",
         "tvg-name" => "CCTV11",
         "group-title" => "央视",
@@ -97,7 +97,7 @@ $tvg_list = [
         "name" => "CCTV-11戏曲"
     ],
     [
-        "vid" => 2000202602,
+        "vid" => 2000202603,
         "tvg-id" => "13",
         "tvg-name" => "CCTV12",
         "group-title" => "央视",
@@ -105,7 +105,7 @@ $tvg_list = [
         "name" => "CCTV-12社会与法"
     ],
     [
-        "vid" => 2000204602,
+        "vid" => 2000204603,
         "tvg-id" => "14",
         "tvg-name" => "CCTV13",
         "group-title" => "央视",
@@ -113,7 +113,7 @@ $tvg_list = [
         "name" => "CCTV-13新闻"
     ],
     [
-        "vid" => 2000204402,
+        "vid" => 2000204403,
         "tvg-id" => "15",
         "tvg-name" => "CCTV14",
         "group-title" => "央视",
@@ -121,7 +121,7 @@ $tvg_list = [
         "name" => "CCTV-14少儿"
     ],
     [
-        "vid" => 2000205002,
+        "vid" => 2000205003,
         "tvg-id" => "16",
         "tvg-name" => "CCTV15",
         "group-title" => "央视",
@@ -129,7 +129,7 @@ $tvg_list = [
         "name" => "CCTV-15音乐"
     ],
     [
-        "vid" => 2000204202,
+        "vid" => 2000204203,
         "tvg-id" => "17",
         "tvg-name" => "CCTV17",
         "group-title" => "央视",
@@ -137,7 +137,7 @@ $tvg_list = [
         "name" => "CCTV-17农业农村"
     ],
     [
-        "vid" => 2000266302,
+        "vid" => 2000266303,
         "tvg-id" => "106",
         "tvg-name" => "CCTV4K",
         "group-title" => "央视",
@@ -145,7 +145,7 @@ $tvg_list = [
         "name" => "CCTV-4K超高清"
     ],
     [
-        "vid" => 2001656802,
+        "vid" => 2001656803,
         "tvg-id" => "20",
         "tvg-name" => "CGTN",
         "group-title" => "央视",
@@ -153,7 +153,7 @@ $tvg_list = [
         "name" => "CGTN"
     ],
     [
-        "vid" => 2000296202,
+        "vid" => 2000296203,
         "tvg-id" => "27",
         "tvg-name" => "湖南卫视",
         "group-title" => "卫视",
@@ -161,7 +161,7 @@ $tvg_list = [
         "name" => "湖南卫视"
     ],
     [
-        "vid" => 2000295502,
+        "vid" => 2000295503,
         "tvg-id" => "28",
         "tvg-name" => "浙江卫视",
         "group-title" => "卫视",
@@ -169,7 +169,7 @@ $tvg_list = [
         "name" => "浙江卫视"
     ],
     [
-        "vid" => 2000295602,
+        "vid" => 2000295603,
         "tvg-id" => "29",
         "tvg-name" => "江苏卫视",
         "group-title" => "卫视",
@@ -177,7 +177,7 @@ $tvg_list = [
         "name" => "江苏卫视"
     ],
     [
-        "vid" => 2000292402,
+        "vid" => 2000292403,
         "tvg-id" => "31",
         "tvg-name" => "东方卫视",
         "group-title" => "卫视",
@@ -185,7 +185,7 @@ $tvg_list = [
         "name" => "东方卫视"
     ],
     [
-        "vid" => 2000294802,
+        "vid" => 2000294803,
         "tvg-id" => "38",
         "tvg-name" => "山东卫视",
         "group-title" => "卫视",
@@ -193,7 +193,7 @@ $tvg_list = [
         "name" => "山东卫视"
     ],
     [
-        "vid" => 2000272102,
+        "vid" => 2000272103,
         "tvg-id" => "30",
         "tvg-name" => "北京卫视",
         "group-title" => "卫视",
@@ -201,7 +201,7 @@ $tvg_list = [
         "name" => "北京卫视"
     ],
     [
-        "vid" => 2000281302,
+        "vid" => 2000281303,
         "tvg-id" => "36",
         "tvg-name" => "辽宁卫视",
         "group-title" => "卫视",
@@ -209,7 +209,7 @@ $tvg_list = [
         "name" => "辽宁卫视"
     ],
     [
-        "vid" => 2000293902,
+        "vid" => 2000293903,
         "tvg-id" => "46",
         "tvg-name" => "黑龙江卫视",
         "group-title" => "卫视",
@@ -217,7 +217,7 @@ $tvg_list = [
         "name" => "黑龙江卫视"
     ],
     [
-        "vid" => 2000292702,
+        "vid" => 2000292703,
         "tvg-id" => "33",
         "tvg-name" => "广东卫视",
         "group-title" => "卫视",
@@ -225,7 +225,7 @@ $tvg_list = [
         "name" => "广东卫视"
     ],
     [
-        "vid" => 2000298002,
+        "vid" => 2000298003,
         "tvg-id" => "32",
         "tvg-name" => "安徽卫视",
         "group-title" => "卫视",
@@ -233,7 +233,7 @@ $tvg_list = [
         "name" => "安徽卫视"
     ],
     [
-        "vid" => 2000292202,
+        "vid" => 2000292203,
         "tvg-id" => "34",
         "tvg-name" => "深圳卫视",
         "group-title" => "卫视",
@@ -241,7 +241,7 @@ $tvg_list = [
         "name" => "深圳卫视"
     ],
     [
-        "vid" => 2000295102,
+        "vid" => 2000295103,
         "tvg-id" => "39",
         "tvg-name" => "天津卫视",
         "group-title" => "卫视",
@@ -249,7 +249,7 @@ $tvg_list = [
         "name" => "天津卫视"
     ],
     [
-        "vid" => 2000297802,
+        "vid" => 2000297803,
         "tvg-id" => "40",
         "tvg-name" => "重庆卫视",
         "group-title" => "卫视",
@@ -257,7 +257,7 @@ $tvg_list = [
         "name" => "重庆卫视"
     ],
     [
-        "vid" => 2000292502,
+        "vid" => 2000292503,
         "tvg-id" => "41",
         "tvg-name" => "东南卫视",
         "group-title" => "卫视",
@@ -265,7 +265,7 @@ $tvg_list = [
         "name" => "东南卫视"
     ],
     [
-        "vid" => 2000294202,
+        "vid" => 2000294203,
         "tvg-id" => "43",
         "tvg-name" => "广西卫视",
         "group-title" => "卫视",
@@ -273,7 +273,7 @@ $tvg_list = [
         "name" => "广西卫视"
     ],
     [
-        "vid" => 2000293402,
+        "vid" => 2000293403,
         "tvg-id" => "45",
         "tvg-name" => "河北卫视",
         "group-title" => "卫视",
@@ -281,7 +281,7 @@ $tvg_list = [
         "name" => "河北卫视"
     ],
     [
-        "vid" => 2000296102,
+        "vid" => 2000296103,
         "tvg-id" => "47",
         "tvg-name" => "河南卫视",
         "group-title" => "卫视",
@@ -289,7 +289,7 @@ $tvg_list = [
         "name" => "河南卫视"
     ],
     [
-        "vid" => 2000294502,
+        "vid" => 2000294503,
         "tvg-id" => "48",
         "tvg-name" => "湖北卫视",
         "group-title" => "卫视",
@@ -297,7 +297,7 @@ $tvg_list = [
         "name" => "湖北卫视"
     ],
     [
-        "vid" => 2000294102,
+        "vid" => 2000294103,
         "tvg-id" => "50",
         "tvg-name" => "江西卫视",
         "group-title" => "卫视",
@@ -305,7 +305,7 @@ $tvg_list = [
         "name" => "江西卫视"
     ],
     [
-        "vid" => 2000284702,
+        "vid" => 2000284703,
         "tvg-id" => "51",
         "tvg-name" => "吉林卫视",
         "group-title" => "卫视",
@@ -313,7 +313,7 @@ $tvg_list = [
         "name" => "吉林卫视"
     ],
 //    [
-//        "vid" => 2000297702,
+//        "vid" => 2000297703,
 //        "tvg-id" => "54",
 //        "tvg-name" => "山西卫视",
 //        "group-title" => "卫视",
@@ -321,7 +321,7 @@ $tvg_list = [
 //        "name" => "山西卫视"
 //    ],
     [
-        "vid" => 2000294402,
+        "vid" => 2000294403,
         "tvg-id" => "55",
         "tvg-name" => "陕西卫视",
         "group-title" => "卫视",
@@ -329,7 +329,7 @@ $tvg_list = [
         "name" => "陕西卫视"
     ],
     [
-        "vid" => 2000294602,
+        "vid" => 2000294603,
         "tvg-id" => "42",
         "tvg-name" => "甘肃卫视",
         "group-title" => "卫视",
@@ -337,7 +337,7 @@ $tvg_list = [
         "name" => "甘肃卫视"
     ],
     [
-        "vid" => 2000295002,
+        "vid" => 2000295003,
         "tvg-id" => "56",
         "tvg-name" => "四川卫视",
         "group-title" => "卫视",
@@ -345,7 +345,7 @@ $tvg_list = [
         "name" => "四川卫视"
     ],
     [
-        "vid" => 2000295402,
+        "vid" => 2000295403,
         "tvg-id" => "58",
         "tvg-name" => "云南卫视",
         "group-title" => "卫视",
@@ -353,7 +353,7 @@ $tvg_list = [
         "name" => "云南卫视"
     ],
     [
-        "vid" => 2000293302,
+        "vid" => 2000293303,
         "tvg-id" => "44",
         "tvg-name" => "贵州卫视",
         "group-title" => "卫视",
@@ -361,7 +361,7 @@ $tvg_list = [
         "name" => "贵州卫视"
     ],
     [
-        "vid"=>2000291502,
+        "vid"=>2000291503,
         "tvg-id" => "37",
         "tvg-name" => "旅游卫视",
         "group-title" => "卫视",
@@ -369,7 +369,7 @@ $tvg_list = [
         "name" => "海南卫视"
     ],
     [
-        "vid" => 2000285102,
+        "vid" => 2000285103,
         "tvg-id" => "53",
         "tvg-name" => "宁夏卫视",
         "group-title" => "卫视",
@@ -377,7 +377,7 @@ $tvg_list = [
         "name" => "宁夏卫视"
     ],
     [
-        "vid" => 2000294702,
+        "vid" => 2000294703,
         "tvg-id" => "59",
         "tvg-name" => "青海卫视",
         "group-title" => "卫视",
@@ -385,7 +385,7 @@ $tvg_list = [
         "name" => "青海卫视"
     ],
 //    [
-//        "vid" => 2000694302,
+//        "vid" => 2000694303,
 //        "tvg-id" => "52",
 //        "tvg-name" => "内蒙古卫视",
 //        "group-title" => "卫视",
@@ -393,7 +393,7 @@ $tvg_list = [
 //        "name" => "内蒙古卫视"
 //    ],
 //    [
-//        "vid" => 2000295202,
+//        "vid" => 2000295203,
 //        "tvg-id" => "71",
 //        "tvg-name" => "西藏卫视",
 //        "group-title" => "卫视",
@@ -401,7 +401,7 @@ $tvg_list = [
 //        "name" => "西藏卫视"
 //    ],
 //    [
-//        "vid" => 2000295302,
+//        "vid" => 2000295303,
 //        "tvg-id" => "57",
 //        "tvg-name" => "新疆卫视",
 //        "group-title" => "卫视",
